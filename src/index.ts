aff70f8450169f57d64cd7f64852e1f9ca49b2e4
Michael Solati <mkslt04@gmail.com>
2021-02-07 15:32:57 -0800

    chore: extract all images

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,54 @@
 import * as glob from 'glob';
 import * as path from 'path';
+import * as fs from 'fs';
 
 import {getWorkingDirectory} from './utils';
 
+const libName = require('../package.json').name;
+const imageMatchRegex = /!\[(.*)\]\((?!http?).*\\*.(png|svg|jpg|jpeg)\)/g;
+const imageRegexGroup = /!\[(?<alt>(.*))\]\((?<src>(?!http?).*\.(png|svg|jpg|jpeg))\)/;
+
 export default async (filepath = process.cwd()): Promise<void> => {
   const workingDirectory = getWorkingDirectory(filepath);
-  const schemasGlob = path.join(workingDirectory, '**', '*.md');
+  const outputFolder = path.join(workingDirectory, libName);
+  const mdGlob = path.join(workingDirectory, '!(node_modules)', '**', '*.md');
+  fs.mkdirSync(outputFolder);
 
-  glob(schemasGlob, {}, async (_, files) => {
+  glob(mdGlob, {}, async (_, files) => {
+    // Go through all the files that were found
     for (const file of files) {
-      console.log(file);
+      const markdown = fs.readFileSync(file, 'utf8');
+      const images = markdown.match(imageMatchRegex);
+
+      // check if images are in file
+      if (images) {
+        console.log(
+          'Extraxting images from:',
+          file.replace(workingDirectory, '')
+        );
+        const markdownPath = path.dirname(file);
+
+        // go through each image found
+        for (const image of images) {
+          // extract image details
+          const groups = imageRegexGroup.exec(image)?.groups;
+
+          // ensure all image details exist
+          if (groups && groups.alt && groups.src) {
+            const imageFileName = groups.src.split('/').pop() || '';
+            const srcPath = path.join(markdownPath, imageFileName);
+            const outputPath = path.join(outputFolder, imageFileName);
+
+            // check if image file exists
+            if (fs.existsSync(srcPath)) {
+              console.log(`Saving ${srcPath} to ${outputPath}`);
+              // fs.createReadStream(srcPath).pipe(
+              //   fs.createWriteStream(outputPath)
+              // );
+            }
+          }
+        }
+      }
     }
   });
 };
